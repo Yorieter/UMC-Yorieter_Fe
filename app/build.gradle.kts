895e5c313c7c06ffb226395b32765d6a88db75e1
이소연 <silvia.sy.lee03@gmail.com>
2024-07-27 14:31:49 +0900

    뷰 모델 빌드 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,11 @@ dependencies {
     //glide
     implementation ("com.github.bumptech.glide:glide:4.13.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.13.0")
+
+    //viewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+
+    //chip
+    implementation("com.google.android.material:material:1.3.0")
 }
