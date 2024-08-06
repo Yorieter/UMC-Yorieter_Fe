61b7ea746dd07fa33133e3b05ba579d0322d5f88
이소연 <silvia.sy.lee03@gmail.com>
2024-08-06 18:37:21 +0900

    feat: add file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,9 @@ dependencies {
 
     //chip
     implementation("com.google.android.material:material:1.3.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
 }
