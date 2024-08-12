dc629fe9711abe99a78bdfba097e4bfbdb4e8cb3
202110894 <ghdekqls821@naver.com>
2024-08-12 21:14:20 +0900

    feat: 로그아웃 API 추가, 회원정보 조회 수정 #14 #15

--- app/src/main/java/com/example/yorieter/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/yorieter/mypage/MypageFragment.kt
@@ -2,7 +2,6 @@ package com.example.yorieter.mypage
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -16,8 +15,9 @@ import androidx.fragment.app.setFragmentResultListener
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.yorieter.login.LoginActivity
-import com.example.yorieter.mypage.api.MyPage
-import com.example.yorieter.mypage.api.MyPageRequest
+import com.example.yorieter.login.api.ResponseData.LogoutResponse
+import com.example.yorieter.login.api.UserRetrofitItf
+import com.example.yorieter.login.api.UserRetrofitObj
 import com.example.yorieter.mypage.api.MypageItf
 import com.example.yorieter.mypage.api.MypageObj
 import com.example.yorieter.mypage.api.ResponseData.GetMypageResponse
@@ -30,7 +30,6 @@ import retrofit2.Response
 class MypageFragment: Fragment() {
 
     lateinit var binding: FragmentMypageBinding
-    private val profileViewModel: ProfileViewModel by activityViewModels()
 
     // SharedPreferences에서 토큰 값 가져오기
     private fun getToken(): String?{
@@ -50,13 +49,6 @@ class MypageFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMypageBinding.inflate(inflater, container, false)
-
-        val userId = getUserId()
-        if (userId != -1){
-            Log.d("사용자 아이디 가져오기 성공", userId.toString())
-        }
-
-        fetchMyProfile(userId) // 마이페이지 조회 API 연동 함수 호출
 
         // 편집 버튼 클릭 시
         binding.mypageEditTv.setOnClickListener {
@@ -108,10 +100,41 @@ class MypageFragment: Fragment() {
 
         // 로그아웃 아이콘 클릭 시
         binding.logoutIv.setOnClickListener {
-            val intent = Intent(requireContext(), LoginActivity::class.java) // 로그인 액티비티로 이동
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-            requireActivity().finish() // 현재 액티비티 종료
+
+            val BEARER_TOKEN = getToken()
+            val authService = UserRetrofitObj.getRetrofit().create(UserRetrofitItf::class.java)
+
+            // 로그아웃 API 연동
+            authService.logout("Bearer $BEARER_TOKEN").enqueue(object: Callback<LogoutResponse>{
+                override fun onResponse(
+                    call: Call<LogoutResponse>,
+                    response: Response<LogoutResponse>
+                ) {
+                    Log.d("RETROFIT/SUCCESS", response.toString())
+                    val resp: LogoutResponse = response.body()!!
+                    if (resp != null) {
+                        if(resp.isSuccess) { // 응답 성공 시
+                            Log.d("LOGOUT/SUCCESS", "로그아웃 성공")
+
+                            val intent = Intent(requireContext(), LoginActivity::class.java) // 로그인 액티비티로 이동
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
+                            requireActivity().finish() // 현재 액티비티 종료
+
+                        } else {
+                            Log.e("LOGOUT/FAILURE", "응답 코드: ${resp.code}, 응답메시지: ${resp.message}")
+                        }
+                    } else {
+                        Log.d("LOGOUT/FAILURE", "Response body is null")
+                    }
+                }
+
+                override fun onFailure(call: Call<LogoutResponse>, t: Throwable) {
+                    Log.d("RETROFIT/FAILURE", t.message.toString())
+                }
+
+            })
+
         }
 
         // 이용약관 아이콘 클릭 시
@@ -129,46 +152,47 @@ class MypageFragment: Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // ViewModel
-        profileViewModel.nickname.observe(viewLifecycleOwner, { nickname ->
-            Log.d("MypageFragment", "닉네임 업데이트: $nickname")
-            binding.myProfileNameTv.text = nickname
-        })
-
-        profileViewModel.introduction.observe(viewLifecycleOwner, { introduction ->
-            Log.d("MypageFragment", "한줄 소개 업데이트: $introduction")
-            binding.myIntroductionTv.text = introduction
-        })
-
-        profileViewModel.profileImageUri.observe(viewLifecycleOwner, { uri ->
-            Log.d("MypageFragment", "프로필 이미지 URI 업데이트: $uri")
-            Glide.with(this)
-                .load(uri)
-                .apply(RequestOptions.circleCropTransform())
-                .into(binding.myProfileIv)
-        })
+    // onResume 메서드는 프래그먼트가 사용자와 상호작용을 재개할 때 호출 됨. 즉, 마이페이지 조회 시 최신 정보를 불러옴
+    override fun onResume() {
+        super.onResume()
+        val token = getToken()
+        val userId = getUserId()
+        if (userId != -1 && token != null) {
+            fetchMyProfile(userId) // 마이페이지 조회 API 연동 함수 호출
+        }
     }
 
     private fun fetchMyProfile(userId: Int){
 
+        val BEARER_TOKEN = getToken()
+
         // 회원정보 조회 API 연동
         val authService = MypageObj.getRetrofit().create(MypageItf::class.java)
-        authService.getMyProfile(userId).enqueue(object: Callback<GetMypageResponse> {
+        authService.getMyProfile("Bearer $BEARER_TOKEN", userId).enqueue(object: Callback<GetMypageResponse> {
             override fun onResponse(
                 call: Call<GetMypageResponse>,
                 response: Response<GetMypageResponse>
             ) {
-                Log.d("MYPAGE/SUCCESS", response.toString())
+                Log.d("RETROFIT/SUCCESS", response.toString())
                 val resp: GetMypageResponse = response.body()!!
                 if (resp != null){
                     if(resp.isSuccess){ // 응답 성공 시
-                        // 닉네임 적용
+
+                        Log.d("MYPAGE/SUCCESS", response.toString())
+
+                        // 응답 값 확인
                         Log.d("nickname", resp.result.nickname)
-                        val resultNickname = resp.result.nickname
-                        binding.myProfileNameTv.text = resultNickname
+                        Log.d("description", resp.result.description)
+                        Log.d("profileUrl", resp.result.profileUrl)
+
+                        // UI 적용
+                        binding.myProfileNameTv.text = resp.result.nickname
+                        binding.myIntroductionTv.text = resp.result.description
+                        Glide.with(this@MypageFragment)
+                            .load(resp.result.profileUrl)
+                            .apply(RequestOptions.circleCropTransform())
+                            .into(binding.myProfileIv)
+
                     } else {
                         Log.e("MYPAGE/FAILURE", "응답 코드: ${resp.code}, 응답메시지: ${resp.message}")
                     }
