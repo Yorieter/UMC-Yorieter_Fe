04e3a9a570ff9dc8b06eda6a9fdbcb31cd39eaa2
202110894 <ghdekqls821@naver.com>
2024-08-12 21:05:09 +0900

    refactor: 회원정보 인터페이스 수정 #15

--- app/src/main/java/com/example/yorieter/mypage/api/MypageItf.kt
+++ app/src/main/java/com/example/yorieter/mypage/api/MypageItf.kt
@@ -1,14 +1,18 @@
 package com.example.yorieter.mypage.api
 
 import com.example.yorieter.mypage.api.ResponseData.GetMyCommentResponse
-import com.example.yorieter.mypage.api.ResponseData.GetMyProfileEditResponse
 import com.example.yorieter.mypage.api.ResponseData.GetMypageResponse
 import com.example.yorieter.mypage.api.ResponseData.GetRecipeResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.PATCH
+import retrofit2.http.Part
+import retrofit2.http.PartMap
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -16,19 +20,25 @@ interface MypageItf {
 
     // 멤버 프로필 조회
     @GET("my-page/{memberId}")
-    fun getMyProfile(@Path(value = "memberId") memberId: Int): Call<GetMypageResponse>
+    fun getMyProfile(
+        @Header("Authorization") token: String,
+        @Path(value = "memberId") memberId: Int
+    ): Call<GetMypageResponse>
 
     // 멤버 프로필 수정
+    @Multipart
     @PATCH("my-page/{memberId}")
     fun changeMyProfile(
         @Header("Authorization") token: String,
         @Path(value = "memberId") memberId: Int,
-        @Body myPage: MyPage
-    ): Call<GetMyProfileEditResponse>
+        @Part image: MultipartBody.Part?,
+        @Part("request") requestBody: RequestBody
+    ): Call<GetMypageResponse>
 
     // 내가 작성한 레시피 목록 조회
     @GET("my-page/{memberId}/recipes")
     fun getMyRecipes(
+        @Header("Authorization") token: String,
         @Path(value = "memberId") memberId: Int,
         @Query(value = "page") page: Int
     ): Call<GetRecipeResponse>
@@ -36,6 +46,7 @@ interface MypageItf {
     // 내가 좋아요한 레시피 목록 조회
     @GET("my-page/{memberId}/recipeLikes")
     fun getMyRecipeLikes(
+        @Header("Authorization") token: String,
         @Path(value = "memberId") memberId: Int,
         @Query(value = "page") page: Int
     ): Call<GetRecipeResponse>
@@ -43,6 +54,7 @@ interface MypageItf {
     // 내가 작성한 댓글 목록 조회
     @GET("my-page/{memberId}/comments")
     fun getMyComments(
+        @Header("Authorization") token: String,
         @Path(value = "memberId") memberId: Int,
         @Query(value = "page") page: Int
     ): Call<GetMyCommentResponse>
