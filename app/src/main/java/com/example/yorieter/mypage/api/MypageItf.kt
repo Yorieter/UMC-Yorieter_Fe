0d8efc1cc144ced584fc49efa07c92659cd86143
202110894 <ghdekqls821@naver.com>
2024-08-16 14:57:07 +0900

    feat: 레시피 상세조회, 삭제, 수정 인터페이스 추가 #15

--- app/src/main/java/com/example/yorieter/mypage/api/MypageItf.kt
+++ app/src/main/java/com/example/yorieter/mypage/api/MypageItf.kt
@@ -1,18 +1,20 @@
 package com.example.yorieter.mypage.api
 
+import com.example.yorieter.mypage.api.ResponseData.DetailResponse
+import com.example.yorieter.mypage.api.ResponseData.DeleteRecipeResponse
+import com.example.yorieter.mypage.api.ResponseData.EditRecipeResponse
 import com.example.yorieter.mypage.api.ResponseData.GetMyCommentResponse
 import com.example.yorieter.mypage.api.ResponseData.GetMypageResponse
 import com.example.yorieter.mypage.api.ResponseData.GetRecipeResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.PATCH
 import retrofit2.http.Part
-import retrofit2.http.PartMap
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -58,4 +60,28 @@ interface MypageItf {
         @Path(value = "memberId") memberId: Int,
         @Query(value = "page") page: Int
     ): Call<GetMyCommentResponse>
+
+    // 레시피 상세 조회
+    @GET("recipes/{recipeId}")
+    fun getRecipeDetail(
+        @Header("Authorization") token: String,
+        @Path(value = "recipeId") recipeId: Int
+    ): Call<DetailResponse>
+
+    // 레시피 삭제
+    @DELETE("recipes/{recipeId}")
+    fun deleteRecipe(
+        @Header("Authorization") token: String,
+        @Path(value = "recipeId") recipeId: Int
+    ): Call<DeleteRecipeResponse>
+
+    // 레시피 수정
+    @Multipart
+    @PATCH("recipes/{recipeId}")
+    fun editRecipe(
+        @Header("Authorization") token: String,
+        @Path(value = "recipeId") recipeId: Int,
+        @Part("request") requestBody: RequestBody,
+        @Part image: MultipartBody.Part?
+    ): Call<EditRecipeResponse>
 }
