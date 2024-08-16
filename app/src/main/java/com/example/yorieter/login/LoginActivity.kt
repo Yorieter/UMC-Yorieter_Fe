2227c5af7fbb1f650b46e4ec4fb3270ae761863a
202110894 <ghdekqls821@naver.com>
2024-08-16 14:51:28 +0900

    refactor: 로그인 API 오류 내용 추가 #14

--- app/src/main/java/com/example/yorieter/login/LoginActivity.kt
+++ app/src/main/java/com/example/yorieter/login/LoginActivity.kt
@@ -18,6 +18,7 @@ import com.example.yorieter.login.api.ResponseData.LoginResponse
 import com.example.yorieter.login.api.LoginClient
 import com.example.yorieter.login.api.UserRetrofitItf
 import com.example.yorieter.login.api.UserRetrofitObj
+import com.example.yorieter.mypage.api.ResponseData.GetMypageResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -96,19 +97,33 @@ class LoginActivity: AppCompatActivity() {
         authService.login(LoginClient(username, password)).enqueue(object: Callback<LoginResponse>{
             override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                 Log.d("LOGIN/SUCCESS", response.toString())
-                val resp: LoginResponse = response.body()!!
-                if (resp != null){
-                    if (resp.isSuccess){
-                        Log.d("아이디 값", resp.result.id.toString())
-                        moveMainActivity(resp) // 로그인 진행
-                    } else {
-                        binding.loginErTv.visibility = View.VISIBLE
-                        Log.e("LOGIN/FAILURE",
-                            "응답 코드: ${resp.code}, 응답메시지: ${resp.message}")
-                        //return
+
+                when (response.code()){
+                    200 -> {
+                        val resp: LoginResponse = response.body()!!
+                        if (resp != null){
+                            if (resp.isSuccess){
+                                Log.d("아이디 값", resp.result.id.toString())
+                                moveMainActivity(resp) // 로그인 진행
+                            } else {
+                                Log.e("LOGIN/FAILURE",
+                                    "응답 코드: ${resp.code}, 응답메시지: ${resp.message}")
+
+                                // code가 MEMBER400일 때 오류 메시지 출력
+                                if (resp.code == "MEMBER401" || response.code() == 500 || response.code() == 400) {
+                                    binding.loginErTv.visibility = View.VISIBLE
+                                }
+                            }
+
+                        } else {
+                            Log.e("실패", response.message())
+                            Log.e("LOGIN/FAILURE", "응답 코드: ${resp.code}, 응답메시지: ${resp.message}")
+
+                        }
                     }
-                } else {
-                    Log.d("LOGIN/FAILURE", "Response body is null")
+                    else -> {
+                        binding.loginErTv.visibility = View.VISIBLE
+                    }
                 }
             }
 
