3e0778d477807e6cbd0caf89131a286d8b87d2d1
202110894 <ghdekqls821@naver.com>
2024-08-16 14:55:44 +0900

    feat: Mypost DB 레시피 아이디 추가 #15

--- app/src/main/java/com/example/yorieter/mypage/dataclass/Mypost.kt
+++ app/src/main/java/com/example/yorieter/mypage/dataclass/Mypost.kt
@@ -3,5 +3,6 @@ package com.example.yorieter.mypage.dataclass
 data class Mypost(
     var coverImg: Int? = null,
     var title: String? = "",
-    var date: String? = ""
+    var date: String? = "",
+    var recipeId: Int // 레시피 아이디 추가
 )
