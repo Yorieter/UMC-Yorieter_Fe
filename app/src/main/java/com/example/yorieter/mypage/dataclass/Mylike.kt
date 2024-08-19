6088d86ccaf2974d6173003fcd75453014fae62d
202110894 <ghdekqls821@naver.com>
2024-08-19 22:41:00 +0900

    feat: Mylike db에 date 변수 추가 #15

--- app/src/main/java/com/example/yorieter/mypage/dataclass/Mylike.kt
+++ app/src/main/java/com/example/yorieter/mypage/dataclass/Mylike.kt
@@ -3,5 +3,6 @@ package com.example.yorieter.mypage.dataclass
 data class Mylike(
     var coverImg: String? = null,
     var title: String? = "",
+    var date: String? = "",
     var recipeId: Int
 )
