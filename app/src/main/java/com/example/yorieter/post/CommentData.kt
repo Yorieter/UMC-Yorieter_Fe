00ee4fb218f7194ef83cf79d9c10e89c0978dafb
202110894 <ghdekqls821@naver.com>
2024-08-22 20:52:47 +0900

    feat: 유저 이미지 변수 타입 변경 #23

--- app/src/main/java/com/example/yorieter/post/CommentData.kt
+++ app/src/main/java/com/example/yorieter/post/CommentData.kt
@@ -1,7 +1,7 @@
 package com.example.yorieter.post
 
 class CommentData (
-    var comment_user_image : Int? = null,
+    var comment_user_image : String? = "",
     var comment_user_name : String? = "",
     var comment_date : String? ="",
     var comment_content : String =""
